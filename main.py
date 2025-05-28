ba50af139b58d2a11cd1bf1cc987a732ead8f9bd
moha0244 <mohamed-lamine.gning@polymtl.ca>
2025-05-28 13:39:23 -0400

    mettre tout le texte en anglais

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import streamlit as st
 from logic import extract_card_block, replace_card_block
 
-st.title("TTC Configuration Copier")
+st.title("TTC Configuration Transfer")
 
-st.subheader(" Carte à copier (dans le fichier source)")
+st.subheader("Source Card (to copy from the source file)")
 source_name = st.text_input("Source CARD Name")
 source_type = st.text_input("Source CARD Type")
 source_slot = st.text_input("Source CARD Slot")
 
 uploaded_source = st.file_uploader("Upload Source XML", type="xml")
 
-st.subheader(" Carte à remplacer (dans le fichier destination)")
+st.subheader("Destination Card (to replace in the destination file)")
 dest_name = st.text_input("Destination CARD Name")
 dest_type = st.text_input("Destination CARD Type")
 dest_slot = st.text_input("Destination CARD Slot")
@@ -22,20 +22,19 @@ if st.button("Copy the configuration"):
         source_lines = uploaded_source.read().decode("utf-8").splitlines()
         dest_lines = uploaded_dest.read().decode("utf-8").splitlines()
 
-
         block = extract_card_block(source_lines, source_name, source_type, source_slot)
         if not block:
-            st.error("Carte source non trouvée.")
+            st.error("Source card not found.")
         else:
 
             modified = replace_card_block(dest_lines, dest_name, dest_type, dest_slot, block)
             if not modified:
-                st.error("Carte destination non trouvée.")
+                st.error("Destination card not found.")
             else:
-                st.success("Bloc copié avec succès.")
-                st.download_button("Télécharger le fichier modifié",
+                st.success("Block successfully copied.")
+                st.download_button("Download modified file",
                                    data="\n".join(modified),
                                    file_name="destination_modified.xml",
                                    mime="text/xml")
     else:
-        st.warning("Veuillez remplir tous les champs et uploader les fichiers.")
+        st.warning("Please fill all fields and upload the files.")
